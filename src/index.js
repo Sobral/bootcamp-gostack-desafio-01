ca7ae807fd8691c4147d23f9ce6f3fd2f8150f9c
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-28 12:04:42 -0400

    Adiciona novo projeto a base de dados

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const config = require('./config/config.json');
 const app = express();
 const PORT = config.port;
 
-app.get('/', (req, res)=>{
-  return res.json({message:"Mensagem recebida com sucesso!"});
+
+app.use(express.json());
+const projects = [];
+
+
+//Adiciona novo projeto a base de projetos
+app.post('/projects', (req, res)=>{
+  const project = req.body;
+  projects.push(project);
+  return res.status(201).json(projects);
 });
 
 app.listen(PORT);
