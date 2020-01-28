79fb81bfb1da8fbfc1b7a4e162b2449b7e6c7fc3
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-28 12:33:09 -0400

    Implementando middleware para checar se id é enviado na requisição

--- src/index.js
+++ src/index.js
@@ -9,8 +9,16 @@ app.use(express.json());
 const projects = [];
 
 
+function checkId(req, res, next){
+  const {id} = req.body;
+  if(!id){
+    return res.status(400).json({message:"Project Id is required!"});
+  }
+  next();
+}
+
 //Adiciona novo projeto a base de projetos
-app.post('/projects', (req, res)=>{
+app.post('/projects', checkId,(req, res)=>{
   const project = req.body;
   projects.push(project);
   return res.status(201).json(projects);
